f50bf088ec16b2d3c42b23ece276614bd83cdffc
ethan11 <ethan11@dd.com>
2018-03-19 00:04:44 +0800

    class loader

--- src/main/java/org/ethan/demo/jvm/d02/CustomizeClassLoader.java
+++ src/main/java/org/ethan/demo/jvm/d02/CustomizeClassLoader.java
@@ -6,7 +6,13 @@ import java.io.*;
 public class CustomizeClassLoader extends ClassLoader {
 
     private String classLoaderName;
+    private String path;
     private String fileExtension = ".class";
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     public CustomizeClassLoader(String classLoaderName) {
         this.classLoaderName = classLoaderName;
     }
@@ -18,6 +24,8 @@ public class CustomizeClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
+        System.out.println("findClass invoked!" + name);
+        System.out.println("classLoader " + classLoaderName);
         byte[] data = this.loadClassData(name);
         return defineClass(name, data, 0, data.length);
     }
@@ -28,7 +36,8 @@ public class CustomizeClassLoader extends ClassLoader {
         ByteArrayOutputStream baos = null;
         try {
 //            classLoaderName = classLoaderName.replaceAll(".", "/");
-            is = new FileInputStream(new File(name + this.fileExtension));
+            name = name.replace(".", File.separator);
+            is = new FileInputStream(new File(this.path + name + this.fileExtension));
             baos = new ByteArrayOutputStream();
             int length = 0;
             while (-1 != (length = is.read())) {
@@ -65,11 +74,37 @@ public class CustomizeClassLoader extends ClassLoader {
     }
 
     public static void main(String[] args) throws Exception {
-        ClassLoader classLoader = new CustomizeClassLoader("My first ClassLoader");
-        System.out.println(classLoader);
-        Class<?> clazz = classLoader.loadClass("org.ethan.demo.jvm.d02.MyTest");
-        System.out.println("==========");
+//        ClassLoader classLoader = new CustomizeClassLoader("My first ClassLoader");
+        CustomizeClassLoader loader = new CustomizeClassLoader(ClassLoader.getSystemClassLoader(), "My first ClassLoader");
+//        loader.testLoader(loader);
+
+        loader.setPath("/home/ethan/Desktop/");
+        Class<?> clazz = loader.loadClass("org.ethan.demo.jvm.d02.MyTest");
+        System.out.println(clazz.hashCode());
         Object object = clazz.newInstance();
         System.out.println(object);
+        System.out.println(object.getClass().getClassLoader());
+
+        System.out.println("========");
+
+
+        CustomizeClassLoader loader2 = new CustomizeClassLoader(ClassLoader.getSystemClassLoader(), "My first ClassLoader");
+//        loader.testLoader(loader);
+
+        loader2.setPath("/home/ethan/Desktop/");
+        Class<?> clazz2 = loader2.loadClass("org.ethan.demo.jvm.d02.MyTest");
+        System.out.println(clazz2.hashCode());
+        Object object2 = clazz2.newInstance();
+        System.out.println(object2);
+        System.out.println(object2.getClass().getClassLoader());
+
+
+    }
+
+    public void testLoader(ClassLoader classLoader) throws Exception {
+        Class<?> clazz = classLoader.loadClass("org.ethan.demo.jvm.d02.MyTest");
+        Object object = clazz.newInstance();
+        System.out.println(object);
+        System.out.println(object.getClass().getClassLoader());
     }
 }
