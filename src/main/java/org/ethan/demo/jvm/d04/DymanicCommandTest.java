78e4a250122ea0baac20639a1824bdb701c716fc
ethan <ethan89798@gmail.com>
2018-10-27 19:16:34 +0800

    字节码指令

---
+++ src/main/java/org/ethan/demo/jvm/d04/DymanicCommandTest.java
@@ -0,0 +1,48 @@
+package org.ethan.demo.jvm.d04;
+
+/**
+ * 方法动态分派
+ *
+ * 方法的动态分派涉及到一个重要概念: 方法接收者
+ * invokevirtual字节码指令的多态查找流程
+ *
+ *
+ * 方法重载(overload)与方法重写(override):
+ *  方法重载是静态的,是编译期行为;
+ *  方法重写是动态的,是运行期行为.
+ *
+ */
+public class DymanicCommandTest {
+
+    public static void main(String[] args) {
+        Fruit apple = new Apple();
+        Fruit orange = new Orange();
+
+        apple.test();
+        orange.test();
+
+        apple = new Orange();
+        apple.test();
+    }
+}
+
+class Fruit {
+
+    public void test() {
+        System.out.println("fruit");
+    }
+}
+
+class Apple extends Fruit {
+    @Override
+    public void test() {
+        System.out.println("apple");
+    }
+}
+
+class Orange extends Fruit {
+    @Override
+    public void test() {
+        System.out.println("orange");
+    }
+}
