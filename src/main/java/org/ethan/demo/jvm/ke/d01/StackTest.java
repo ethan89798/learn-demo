599ebdc3dbc34cf72e10b852dfff90954573d186
ethan <ethan89798@gmail.com>
2018-12-16 10:23:15 +0800

    一些Jvm的参数学习

--- src/main/java/org/ethan/demo/jvm/ke/d01/StackTest.java
+++ src/main/java/org/ethan/demo/jvm/ke/d01/StackTest.java
@@ -1,7 +1,7 @@
 package org.ethan.demo.jvm.ke.d01;
 
 /**
- * 设置参数 -Xss256k 设置栈大小
+ * 设置参数 -Xss256k 设置栈大小(默认值为1M)
  * java.lang.StackOverflowError
  *  一般出现在递归的情况比较多.
  *  递归与非递归,如果在树的实现时,递归代码简洁,但非递归的代码复杂但性能高
