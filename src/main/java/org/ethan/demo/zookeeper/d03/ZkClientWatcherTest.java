9a0b09cbc29bc633819f8eef704c4d941f7e5536
ethan <ethan-huang@faxmail.com>
2019-07-09 23:34:32 +0800

    增加判断

--- src/main/java/org/ethan/demo/zookeeper/d03/ZkClientWatcherTest.java
+++ src/main/java/org/ethan/demo/zookeeper/d03/ZkClientWatcherTest.java
@@ -31,19 +31,28 @@ public class ZkClientWatcherTest {
         });
 
         Thread.sleep(2000);
-        client.createPersistent("/super-watcher");
+        if (!client.exists("/super-watcher")) {
+            client.createPersistent("/super-watcher");
+        }
         Thread.sleep(1000);
-        client.createPersistent("/super-watcher/c1", "c1内容");
+        if (!client.exists("/super-watcher/c1")) {
+            client.createPersistent("/super-watcher/c1", "c1内容");
+        }
         Thread.sleep(1000);
-        client.createPersistent("/super-watcher/c2", "c2内容");
+        if (!client.exists("/super-watcher/c2")) {
+            client.createPersistent("/super-watcher/c2", "c2内容");
+        }
         Thread.sleep(1000);
-        client.delete("/super-watcher");
+//        client.delete("/super-watcher");
         Thread.sleep(1000);
         client.deleteRecursive("/super-watcher");
     }
 
     private static void testClientWatcherData() throws InterruptedException {
         ZkClient client = new ZkClient(new ZkConnection("192.168.1.210:2181"), 3000);
+        if (!client.exists("/super-watcher2")) {
+            client.createEphemeral("/super-watcher2", "test watcher2");
+        }
         client.subscribeDataChanges("/super-watcher2", new IZkDataListener() {
             @Override
             public void handleDataChange(String dataPath, Object data) throws Exception {
@@ -60,6 +69,6 @@ public class ZkClientWatcherTest {
         client.writeData("/super-watcher2", "test data".getBytes(), -1);
         Thread.sleep(1000);
         client.delete("/super-watcher2");
-        Thread.sleep(100000);
+        Thread.sleep(1000);
     }
 }
